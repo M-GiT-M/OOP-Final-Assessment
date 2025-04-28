a2b59e5fdd538fabd08bbf43aabc0b0dece3b967
Mahmoud Mustafa <u54mm24@abdn.ac.uk>
2025-04-28 20:52:36 +0300

    Integrated the Combat Dialogue

--- RPG Game.py
+++ RPG Game.py
@@ -56,3 +56,40 @@ class Enemy:
     def take_damage(self, damage):
         self.hp -= damage
         print(f"{self.name} takes {damage} Damage! - Current HP: {self.hp}")
+
+def combat(player, enemy):
+    print(f"\n {enemy.name} has Appeared!")
+    while player.is_alive() and enemy.is_alive():
+        #Player's move
+        print("\n Choose your move:")
+        print("1 - Attack")
+        print("2 - Open Inventory")
+        print("3 - Escape")
+        move = input("Enter a Number: ")
+
+        if move == "1":
+            player.attack(enemy)
+        elif move == "2":
+            player.show_inventory()
+            continue
+            #Allows the player to decide how to play their next move after checking the inventory
+        elif move == "3":
+            if random.random() < 0.5:
+                #Generates a random number between 0-1 as a 50% chance of escaping
+                print("Successful Escape!")
+                return
+            else:
+                print("Failed Escape")
+        else:
+            print("Invalid Action. Please enter a number between 1-3.")
+            continue
+
+        #If enemy is alive, their turn proceeds
+        if enemy.is_alive():
+            print(f"\n{enemy.name}'s turn:")
+            enemy.attack(player)
+
+    if player.is_alive():
+        print(f"\nCongratulations! You have defeated the {enemy.name}!")
+    else:
+        print("\nYou have been defeated. Good Try!")
