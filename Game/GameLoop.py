66ac55641501386de3ced5c69f6ba6db413564e0
Mahmoud Mustafa <u54mm24@abdn.ac.uk>
2025-04-30 00:46:52 +0300

    Completed the Full Game Loop

--- Game/GameLoop.py
+++ Game/GameLoop.py
@@ -3,6 +3,7 @@ from Classing.Enemy import Enemy, Ogre, Witch, Giant
 from Classing.Item import Item
 from SaveLoad.saveload import save_game, load_game
 from .Combat import combat
+from .Crafting import get_all_recipes
 
 def main_menu():
     #Creating a loop of the game
@@ -45,4 +46,98 @@ def main_menu():
             print("Invalid Option - Please Choose one of the Listed Options")
 
 def game_loop(player):
-    pass
+    #Main loop for the adventure game
+    recipes = get_all_recipes()
+
+    while player.is_alive():
+        print("\n" + "=" * 25)
+        print(f"ADVENTURE MENU - {player.name}")
+        print(f"HP: {player.hp}/{player.max_hp} | Strength: {player.strength}")
+        print("=" * 25)
+        print("1 - Battle Ogre")
+        print("2 - Battle Witch")
+        print("3 - Battle Giant")
+        print("4 - Open Inventory")
+        print("5 - Use Item")
+        print("6 - Equip Item")
+        print("7 - Craft Item")
+        print("8 - Save Game")
+        print("9 - Return to Main Menu") 
+
+        choice = input("Enter your Option: ")
+
+        #Battle with an Ogre
+        if choice == "1":
+            result = combat(player, Ogre())
+            if result == "Defeat":
+                print("Game Over!")
+                break
+
+        #Battle with a Witch
+        if choice == "2":
+            result = combat(player, Witch())
+            if result == "Defeat":
+                print("Game Over!")
+                break
+
+        #Batlle with a Giant
+        if choice == "3":
+            result = combat(player, Giant())
+            if result == "Defeat":
+                print("Game Over!")
+                break
+
+        #Open Inventory
+        elif choice == "4":
+            player.show_inventory()
+
+        #Use Item - i.e. Consumables
+        elif choice == "5":
+            player.show_inventory()
+            if player.inventory:
+                try:
+                    item_index = int(input("Enter Item Number to Use (0 to Cancel): ")) - 1
+                    if item_index != -1:
+                        player.use_item(item_index)
+                except ValueError:
+                    print("Invalid Option - Please Enter a Valid Number.")
+
+        #Equip Item - i.e. Armor, Weapon
+        elif choice == "6":
+            player.show_inventory()
+            if player.inventory:
+                try:
+                    item_index = int(input("Enter Item Number to Use (0 to Cancel): ")) - 1
+                    if item_index != -1:
+                        player.equip_item(item_index)
+                except ValueError:
+                    print("Invalid Option - Please Enter a Valid Number.")
+
+        #Craft an Item 
+        elif choice == "7":
+            #Display all available items that can be crafted
+            print("\n======= CRAFTING RECIPES =======")
+            recipe_names = list(recipes.keys())
+            for i, recipe_name in enumerate(recipe_names):
+                recipe = recipes[recipe_name]
+                print(f"{i+1} - {recipe_name}, Requires: {', '.join(recipe.crafting_items)}")
+
+            #Taking player's choice
+            try:
+                recipe_index = int(input("\n Enter Recipe Number to Craft (0 to Cancel): ")) - 1
+                if 0 <= recipe_index < len(recipe_names):
+                    recipe_name = recipe_names[recipe_index]
+                    player.craft_item(recipe_name, recipes)
+            except ValueError:
+                print("Inavlid Option - Please Enter a Valid Number.")
+
+        #Saving the Game
+        elif choice == "8":
+            save_game(player)
+
+        #Main Menu
+        elif choice == "9":
+            break
+
+        else:
+            print("Invalid Choice - Please enter a number between 1-9.")
