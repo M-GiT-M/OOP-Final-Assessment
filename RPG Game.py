ba1a920927cc44606661cc8af3c3ae6c98a6a4d3
Mahmoud Mustafa <u54mm24@abdn.ac.uk>
2025-04-29 20:58:37 +0300

    Loading Game from a File - Complete

--- RPG Game.py
+++ RPG Game.py
@@ -1,6 +1,7 @@
 import random
 from abc import ABC, abstractmethod
 import json
+import os
 
 class Item: 
     def __init__(self, name, description = "", attack = 0, defense = 0, consumable = False, effect = None, effect_amount = 0, crafting_items = None):
@@ -429,7 +430,7 @@ class Giant(Enemy):
                 character.take_damage(reduced_damage)
 
 #Saving the Game
-def save_game(player, filename = "save_game.json"):
+def save_game(player, filename = "saved_game.json"):
     #Saving the current game statistics and details to a file
     try:
         #Converting the player to dictionary
@@ -444,6 +445,31 @@ def save_game(player, filename = "save_game.json"):
     except Exception as e:
         print(f"Having Trouble Saving the Game: {e}")
         return False
+    
+#Loading a Saved Game from a File
+def load_game(filename = "saved_game.json"):
+    #Loading a game from a file
+    try:
+        #Checks for file existance
+        if not os.path.exists(filename):
+            print(f"Game File {filename} - Not Found!")
+            return None
+        
+        #Loading the game
+        with open(filename, 'r') as f:
+            player_details = json.load(f)
+
+        #'Restoring' player details
+        player = Character.save_character(player_details)
+
+        print(f"Game File {filename} - Loaded Successfully!")
+        return player
+    except json.JSONDecodeError:
+        print(f"{filename} Error: INVALID JSON DATA.")
+        return None
+    except Exception as e:
+        print(f"Error: UNABLE TO LOAD GAME {e}")
+        return None
             
 def combat(player, enemy):
     print(f"\n {enemy.name} has Appeared!")
